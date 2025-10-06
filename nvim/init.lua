cca3f27e1a64930f7f952837a3424230108a2a79
initialrise <98199146+initialrise@users.noreply.github.com>
2025-10-06 10:10:33 +0545

    refactor: nvim diagnostics display mapped to leader d

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,20 +4,35 @@ vim.g.maplocalleader=' '
 require("config.lazy")
 
 -- diagnostic toggler
-local diagnostics_active = true
+-- local diagnostics_active = true
 
-local function toggle_diagnostics()
-  diagnostics_active = not diagnostics_active
-  if diagnostics_active then
-    vim.diagnostic.enable()
-    print("Diagnostics enabled")
-  else
-    vim.diagnostic.disable()
-    print("Diagnostics disabled")
-  end
-end
+-- local function toggle_diagnostics()
+--   diagnostics_active = not diagnostics_active
+--   if diagnostics_active then
+--     vim.diagnostic.enable()
+--     print("Diagnostics enabled")
+--   else
+--     vim.diagnostic.disable()
+--     print("Diagnostics disabled")
+--   end
+-- end
+--
+-- vim.keymap.set('n', '<leader>d', toggle_diagnostics, { noremap = true, silent = true, desc = "Toggle diagnostics" })
+--
+-- diagnostic config
+-- Disable virtual text, keep signs and underline
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
 
-vim.keymap.set('n', '<leader>d', toggle_diagnostics, { noremap = true, silent = true, desc = "Toggle diagnostics" })
+vim.keymap.set("n", "<leader>d", function()
+  vim.diagnostic.open_float(nil, { border = "rounded", scope = "cursor" })
+end, { desc = "Show diagnostic message" })
+
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
